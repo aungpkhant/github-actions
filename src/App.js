8c4c01fa76943ad3afc3be56ed52a5be33b7b20e
derrick-aung-khant <derrick.a.h@marathonmyanmar.com>
2021-03-09 19:04:20 +0630

    Test rebuild with github actions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am awesome so are you.
         </p>
         <a
           className="App-link"
